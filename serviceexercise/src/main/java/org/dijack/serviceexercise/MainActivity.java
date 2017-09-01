3fe28643ec77429bb3ab01f35336e771c137e501
Dijack Dong <dijack.dong@hp.com>
2017-09-01 21:48:35 +0800

    Add ServiceExercise app

---
+++ serviceexercise/src/main/java/org/dijack/serviceexercise/MainActivity.java
@@ -0,0 +1,23 @@
+package org.dijack.serviceexercise;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View view) {
+        startService(new Intent(this, MyService.class));
+    }
+
+    public void stopService(View view) {
+        stopService(new Intent(this, MyService.class));
+    }
+}
